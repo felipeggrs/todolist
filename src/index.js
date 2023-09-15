1a1aefcaa14240431abc9fabb918898e88c0c0c4
felipe <felipeggrs@gmail.com>
2023-09-15 13:03:22 -0300

    Start project and add/install the initial setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import "./styles.css";
+// import { formatDistance, subDays } from "date-fns";
 
-console.log("The index.js file and webpack are working correctly!");
+// create todo list
+function createList(title) {
+  return { title, items: [] };
+}
+
+// create todo item
+function createItem(title, description, dueDate, priority) {
+  return { title, description, dueDate, priority, status: "Pending" };
+}
+
+// put the item inside the chosen list
+function assignItemToList(todoItem, list) {
+  list.items.push(todoItem);
+}
+
+const defaultList = createList("default list");
+const defaultItem = createItem(
+  "Wake up",
+  "Time to leave the bed",
+  "September 20, 2023",
+  "High"
+);
+
+assignItemToList(defaultItem, defaultList);
+console.log(defaultList);
